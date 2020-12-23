8e37d52edba2c024b21f62d4b53b2b13f0fc2f8a
OlegLototsky <oleg.lototsky94@gmail.com>
2020-12-24 00:31:32 +0300

    Добавьте файлы проекта.

---
+++ Программа курса денег/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Программа_курса_денег
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AUD",
+            "GBP",
+            "BYR",
+            "DKK",
+            "USD",
+            "EUR",
+            "ISK",
+            "KZT",
+            "CAD",
+            "NOK",
+            "XDR",
+            "SGD",
+            "TRL",
+            "UAH",
+            "SEK",
+            "CHF",
+            "JPY"});
+            this.comboBox1.Location = new System.Drawing.Point(225, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(175, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите валюту";
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(406, 58);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(175, 51);
+            this.btn_search.TabIndex = 4;
+            this.btn_search.Text = "Поиск";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(587, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(617, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ЦБ на сегодня";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Программа курса денег/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Net;
+using System.Xml.Linq;
+
+
+namespace Программа_курса_денег
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           InitializeComponent();
+           Parse();
+           
+           
+
+        }
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            string s_name = comboBox1.Text;
+
+            var dollar = Currencies.Where(x => x.CharCode == s_name).FirstOrDefault();
+
+            textBox1.Text =  dollar.Price;
+
+        }
+
+        public void Parse()
+        {
+         
+        DateTime timebank = new DateTime(2020, 12, 24);
+
+            string url = $"http://www.cbr.ru/scripts/XML_daily.asp?date_req={timebank.ToString("dd/MM/yyyy")}";
+
+            WebClient wc = new WebClient();
+            wc.Encoding = System.Text.Encoding.UTF8;
+
+            string BankString = wc.DownloadString(url);
+
+            XmlDocument urlbank = new XmlDocument();
+            urlbank.LoadXml(BankString);
+
+            XmlNodeList XmlBankList = urlbank.GetElementsByTagName("Valute");
+            foreach (XmlNode node in XmlBankList)
+            {
+                Currency currency = new Currency()
+                {
+                    Name = node["Name"].InnerText,
+                    CharCode = node["CharCode"].InnerText,
+                    Price = node["Value"].InnerText, // нужно переделать под decimal
+                   };
+
+                Currencies.Add(currency);
+            }
+      
+        }
+
+        public List<Currency> Currencies { get; } = new List<Currency>();
+        public class Currency
+        { 
+            public string CharCode { get; set; }
+
+            public string Name { get; set; }
+
+            private decimal  price;
+
+            public string Price
+            {
+                get { return price.ToString("c"); }
+                set { price = Convert.ToDecimal(value); }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
