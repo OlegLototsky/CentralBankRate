a36a83de64b7a32825b5de618355ccb114024049
OlegLototsky <oleg.lototsky94@gmail.com>
2020-12-24 14:47:22 +0300

    Первый этап

--- Программа курса денег/Form1.Designer.cs
+++ Программа курса денег/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Программа_курса_денег
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -34,30 +34,36 @@ namespace Программа_курса_денег
             this.btn_search = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_clear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "AUD",
             "GBP",
-            "BYR",
+            "BYN",
             "DKK",
             "USD",
             "EUR",
-            "ISK",
+            "INR",
             "KZT",
             "CAD",
             "NOK",
             "XDR",
             "SGD",
-            "TRL",
+            "TRY",
             "UAH",
             "SEK",
             "CHF",
             "JPY"});
-            this.comboBox1.Location = new System.Drawing.Point(225, 70);
+            this.comboBox1.Location = new System.Drawing.Point(56, 70);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(175, 28);
             this.comboBox1.TabIndex = 0;
@@ -65,7 +71,7 @@ namespace Программа_курса_денег
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(242, 47);
+            this.label1.Location = new System.Drawing.Point(77, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 20);
             this.label1.TabIndex = 1;
@@ -73,7 +79,7 @@ namespace Программа_курса_денег
             // 
             // btn_search
             // 
-            this.btn_search.Location = new System.Drawing.Point(406, 58);
+            this.btn_search.Location = new System.Drawing.Point(250, 58);
             this.btn_search.Name = "btn_search";
             this.btn_search.Size = new System.Drawing.Size(175, 51);
             this.btn_search.TabIndex = 4;
@@ -83,33 +89,78 @@ namespace Программа_курса_денег
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(587, 70);
+            this.textBox1.Location = new System.Drawing.Point(617, 70);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(175, 26);
             this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(617, 47);
+            this.label2.Location = new System.Drawing.Point(647, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(120, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "ЦБ на сегодня";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // Form1
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(865, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ЦБ вчера";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(816, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(175, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(680, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(436, 58);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(175, 51);
+            this.btn_clear.TabIndex = 10;
+            this.btn_clear.Text = "Очистить";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1083, 450);
+            this.ClientSize = new System.Drawing.Size(1083, 175);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btn_search);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Курс в Центральном банке";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +173,10 @@ namespace Программа_курса_денег
         private System.Windows.Forms.Button btn_search;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_clear;
     }
 }
 
--- Программа курса денег/Form1.cs
+++ Программа курса денег/Form1.cs
@@ -14,32 +14,30 @@ using System.Xml.Linq;
 
 namespace Программа_курса_денег
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
            InitializeComponent();
            Parse();
-           
+            ParseOLD();
+            Colors();
            
 
         }
-        private void btn_search_Click(object sender, EventArgs e)
-        {
-            string s_name = comboBox1.Text;
-
-            var dollar = Currencies.Where(x => x.CharCode == s_name).FirstOrDefault();
-
-            textBox1.Text =  dollar.Price;
-
+        
+        public void Colors()
+        { 
+        
+        
         }
 
-        public void Parse()
+        public void Parse() // загрузка данных из ЦБ на сегоднящний день
         {
-         
-        DateTime timebank = new DateTime(2020, 12, 24);
 
-            string url = $"http://www.cbr.ru/scripts/XML_daily.asp?date_req={timebank.ToString("dd/MM/yyyy")}";
+            string timebank = DateTime.Today.ToString("dd/MM/yyyy");
+
+            string url = $"http://www.cbr.ru/scripts/XML_daily.asp?date_req={timebank}";
 
             WebClient wc = new WebClient();
             wc.Encoding = System.Text.Encoding.UTF8;
@@ -56,7 +54,7 @@ namespace Программа_курса_денег
                 {
                     Name = node["Name"].InnerText,
                     CharCode = node["CharCode"].InnerText,
-                    Price = node["Value"].InnerText, // нужно переделать под decimal
+                    Price = Convert.ToDecimal(node["Value"].InnerText), // нужно переделать под decimal
                    };
 
                 Currencies.Add(currency);
@@ -64,25 +62,78 @@ namespace Программа_курса_денег
       
         }
 
+        public void ParseOLD() // загрузка данных из ЦБ на вчерашний день
+        {
+
+            string OldTimeBank = DateTime.Today.AddDays(-1).ToString("dd/MM/yyyy");
+
+            string url = $"http://www.cbr.ru/scripts/XML_daily.asp?date_req={OldTimeBank}";
+
+            WebClient wc = new WebClient();
+            wc.Encoding = System.Text.Encoding.UTF8;
+
+            string BankString = wc.DownloadString(url);
+
+            XmlDocument urlbank = new XmlDocument();
+            urlbank.LoadXml(BankString);
+
+            XmlNodeList XmlBankList = urlbank.GetElementsByTagName("Valute");
+            foreach (XmlNode node in XmlBankList)
+            {
+                Currency currency1 = new Currency()
+                {
+                    Name = node["Name"].InnerText,
+                    CharCode = node["CharCode"].InnerText,
+                    Price = Convert.ToDecimal(node["Value"].InnerText), // нужно переделать под decimal
+                };
+
+                CurrenciesOLD.Add(currency1);
+            }
+
+        }
+
+        private void btn_search_Click(object sender, EventArgs e) //кнопка поиска
+        {
+            string sName = comboBox1.Text;
+
+            var CharCodePrice = Currencies.Where(x => x.CharCode == sName).FirstOrDefault();
+
+            var OldCharCodePrice = CurrenciesOLD.Where(x => x.CharCode == sName).FirstOrDefault();
+
+            if (string.IsNullOrEmpty(this.comboBox1.Text))
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                MessageBox.Show("Ошибка. Выберите валюту ", "Сообщение");
+            }
+            else
+            {
+                textBox1.Text = CharCodePrice.Price.ToString("G");
+
+                textBox2.Text = OldCharCodePrice.Price.ToString("G");
+            }
+
+        }
+
+        private void btn_clear_Click(object senderr, EventArgs e) // Кнопка очистки
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.SelectedIndex = -1;
+        }
+
         public List<Currency> Currencies { get; } = new List<Currency>();
+
+        public List<Currency> CurrenciesOLD { get; } = new List<Currency>();
         public class Currency
         { 
             public string CharCode { get; set; }
 
             public string Name { get; set; }
 
-            private decimal  price;
-
-            public string Price
-            {
-                get { return price.ToString("c"); }
-                set { price = Convert.ToDecimal(value); }
-            }
+            public decimal Price { get; set; }
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
+      
     }
 }
